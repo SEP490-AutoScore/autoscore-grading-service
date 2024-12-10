4a93d269f6f8255f7e01f9e8b4e76a384d430227
votrongvuong672 <votrongvuong001@gmail.com>
2024-12-10 11:35:36 +0700

    fix

--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/AutoScoreApplication.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/AutoScoreApplication.java
@@ -2,12 +2,10 @@ package com.CodeEvalCrew.AutoScore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
-
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.CodeEvalCrew.AutoScore")
@@ -16,11 +14,12 @@ public class AutoScoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoScoreApplication.class, args);
-	}
-		//gemini ai
-		@Bean
-		public RestTemplate restTemplate() {
-			return new RestTemplate();
-		}
 
+	}
+
+	// gemini ai
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/repositories/examdatabase_repository/IExamDatabaseRepository.java
+++ AutoScore/src/main/java/com/CodeEvalCrew/AutoScore/repositories/examdatabase_repository/IExamDatabaseRepository.java
@@ -1,4 +1,5 @@
 package com.CodeEvalCrew.AutoScore.repositories.examdatabase_repository;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,13 +12,13 @@ import com.CodeEvalCrew.AutoScore.models.Entity.Exam_Database;
 @Repository
 public interface IExamDatabaseRepository extends JpaRepository<Exam_Database, Long> {
     Exam_Database findByExamPaperExamPaperId(Long examPaperId);
-        // Fetch only the databaseName based on examPaperId
+
+    // Fetch only the databaseName based on examPaperId
     @Query("SELECT e.databaseName FROM Exam_Database e WHERE e.examPaper.examPaperId = :examPaperId")
     String findDatabaseNameByExamPaperId(@Param("examPaperId") Long examPaperId);
 
-       // Tìm Exam_Database dựa vào examQuestionId
-       @Query("SELECT e FROM Exam_Database e JOIN e.examPaper p JOIN p.examQuestions q WHERE q.examQuestionId = :examQuestionId")
-       Optional<Exam_Database> findByExamQuestionId(@Param("examQuestionId") Long examQuestionId);
-   
-       Optional<Exam_Database> findByExamPaper_ExamPaperId(Long examPaperId);
+    @Query("SELECT e FROM Exam_Database e JOIN e.examPaper p JOIN p.examQuestions q WHERE q.examQuestionId = :examQuestionId")
+    Optional<Exam_Database> findByExamQuestionId(@Param("examQuestionId") Long examQuestionId);
+
+    Optional<Exam_Database> findByExamPaper_ExamPaperId(Long examPaperId);
 }
